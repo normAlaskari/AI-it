5e0d46233910915436e3fd71101415c19ae9c527
Norman Alaskari <noor.ask@hotmail.com>
2023-02-06 20:55:13 -0600

    added styling

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 chrome.tabs.executeScript({
   code: "window.getSelection().toString();"
 }, function(selection) {
-  document.getElementById("textarea").value = selection[0];
+  document.getElementById("text").value = selection[0];
 });
