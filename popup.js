90aedecc2282d1ee4f9347e0a109a0a604a08d27
Norman Alaskari <noor.ask@hotmail.com>
2023-02-06 20:16:10 -0600

    print selected text and print on the screen now works

--- popup.js
+++ popup.js
@@ -1,14 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-  try {
-    const selectedText = window.getSelection().toString();
-    if (selectedText === '') {
-      throw new Error('No text is selected.');
-    }
-
-    const selectedTextContainer = document.querySelector('#selected-text');
-    selectedTextContainer.textContent = selectedText;
-  } catch (error) {
-    const errorMessage = error.message || 'An error occurred while trying to get the selected text.';
-    document.querySelector('#selected-text').textContent = errorMessage;
-  }
+chrome.tabs.executeScript({
+  code: "window.getSelection().toString();"
+}, function(selection) {
+  document.getElementById("textarea").value = selection[0];
 });
